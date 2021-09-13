28b2dab440d2f4ffb106506946e7814ecbe59d9a
afolabiopakunle <opakunleafolabi@gmail.com>
2021-09-13 21:03:46 +0100

    Tested form with @ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,10 +7,11 @@ import { NgForm } from '@angular/forms';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+  @ViewChild('myForm') signUpForm: NgForm;
   
   username;
 
-  onSubmit(form: NgForm) {
-    console.log(form.value)
+  onSubmit() {
+    console.log(this.signUpForm.value)
   }
 }
